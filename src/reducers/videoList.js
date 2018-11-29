189b9eba356746d259fab8661dc966a276bdc9c9
ghostrib <sh4rkbot@gmail.com>
2018-11-29 10:38:07 -0600

    implement root reducer

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,15 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
+var videoListReducer = (state = [], action) => {
   //TODO: define a reducer for the videoList field of our state.
+  let newState = [];
+  switch (action.type) {
+    case 'CHANGE_VIDEO_LIST':
+      newState = [...action.videos];
+      return newState;
+    default:
+      return state;
+  }
 };
 
 export default videoListReducer;
--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
-let store = createStore(rootReducer);
+let store = createStore(rootReducer, applyMiddleware(thunk));
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+export default store;
